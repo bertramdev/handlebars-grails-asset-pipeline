548cedac453a52768d6eb389f8804390e8f5efc8
David Estes <davydotcom@gmail.com>
2014-01-17 16:48:44 -0500

    cleanup

--- src/groovy/asset/pipeline/handlebars/HandlebarsAssetFile.groovy
+++ src/groovy/asset/pipeline/handlebars/HandlebarsAssetFile.groovy
@@ -2,60 +2,16 @@ package asset.pipeline.handlebars
 
 import asset.pipeline.CacheManager
 import asset.pipeline.AssetHelper
+import asset.pipeline.AbstractAssetFile
 
 
-class HandlebarsAssetFile {
+class HandlebarsAssetFile extends AbstractAssetFile {
 	static final String contentType = 'application/javascript'
 	static extensions = ['handlebars', 'hbs']
 	static final String compiledExtension = 'js'
 	static processors = [HandlebarsProcessor]
 
-
-	// Cache For MD5
-	static cache = [:]
-
-	File file
-	def baseFile
-	def encoding
-
-	HandlebarsAssetFile(file, baseFile=null) {
-		this.file = file
-		this.baseFile = baseFile
-	}
-
-	def processedStream(precompiler=false) {
-		def fileText
-		if(baseFile?.encoding || encoding) {
-			fileText = file?.getText(baseFile?.encoding ? baseFile.encoding : encoding)
-		} else {
-			fileText = file?.text
-		}
-
-		def md5 = AssetHelper.getByteDigest(fileText.bytes)
-		if(!precompiler) {
-			def cache = CacheManager.findCache(file.canonicalPath, md5)
-			if(cache) {
-				return cache
-			}
-		}
-
-		for(processor in processors) {
-			def processInstance = processor.newInstance(precompiler)
-			fileText = processInstance.process(fileText, this)
-		}
-
-
-		if(!precompiler) {
-			CacheManager.createCache(file.canonicalPath,md5,fileText)
-		}
-
-		return fileText
-		// Return File Stream
-	}
-
-
 	def directiveForLine(line) {
 		return null
-		// line.find(/#=(.*)/) { fullMatch, directive -> return directive }
 	}
 }
