01a121588e66de4859d1a3fb98c3a56f187c3633
David Estes <davydotcom@gmail.com>
2014-02-18 11:21:51 -0500

    1.3.0 release

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,3 +1,10 @@
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+
+
 eventAssetPrecompileStart = { assetConfig ->
 	assetConfig.specs << 'asset.pipeline.handlebars.HandlebarsAssetFile'
+	def grailsApplication = ApplicationHolder.getApplication()
+	if(!grailsApplication.config.grails.assets.plugin."handlebars-asset-pipeline".excludes || grailsApplication.config.grails.assets.plugin."handlebars-asset-pipeline".excludes.size() == 0) {
+		grailsApplication.config.grails.assets.plugin."handlebars-asset-pipeline".excludes = ["handlebars/**/*"]
+	}
 }
