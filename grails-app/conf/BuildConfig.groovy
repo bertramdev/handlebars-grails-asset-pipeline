b63104e3e1e0b6ae1cf37cf2939b7eb3d46a6f53
David Estes <davydotcom@gmail.com>
2015-05-11 09:36:15 -0400

    bumping to core 2.1.3 of handlebars asset-pipeline adapter and disconnecting version from equivalent handlebars version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
 
 	inherits 'global'
@@ -12,15 +13,15 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.1.2"
+		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.1.3"
 	}
 
 	plugins {
 
-		runtime ":asset-pipeline:2.1.1"
+		runtime ":asset-pipeline:2.2.0"
 
 
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.0.1', ':rest-client-builder:2.0.1', {
 			export = false
 		}
 	}
