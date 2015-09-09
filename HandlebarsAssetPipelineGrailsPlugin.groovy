6149eaac752dcf95858026bb607ca4616346ea46
David Estes <davydotcom@gmail.com>
2015-09-09 09:02:41 -0400

    version bump for release

--- HandlebarsAssetPipelineGrailsPlugin.groovy
+++ HandlebarsAssetPipelineGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import asset.pipeline.handlebars.HandlebarsAssetFile
 
 class HandlebarsAssetPipelineGrailsPlugin {
 
-    def version = "2.1.4"
+    def version = "2.5.0"
 
     def grailsVersion = "2.0 > *"
     def title = "Handlebars Asset-Pipeline Plugin"
