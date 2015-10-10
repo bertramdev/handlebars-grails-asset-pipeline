a62286a1bc192f650656b580e99f950cd9ed7616
David Estes <davydotcom@gmail.com>
2015-10-10 11:03:28 -0400

    udpating version

--- HandlebarsAssetPipelineGrailsPlugin.groovy
+++ HandlebarsAssetPipelineGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import asset.pipeline.handlebars.HandlebarsAssetFile
 
 class HandlebarsAssetPipelineGrailsPlugin {
 
-    def version = "2.5.1"
+    def version = "2.6.0"
 
     def grailsVersion = "2.0 > *"
     def title = "Handlebars Asset-Pipeline Plugin"
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,12 +15,13 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.6.0"
+		compile 'org.mozilla:rhino:1.7R4'
 	}
 
 	plugins {
 
 		runtime ":asset-pipeline:2.6.0"
-		compile 'org.mozilla:rhino:1.7R4'
+		
 
 		build ':release:3.1.1', ':rest-client-builder:2.0.1', {
 			export = false
