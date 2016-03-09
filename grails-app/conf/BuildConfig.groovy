2768344f64594a4cbfe9f2f998c3dc67a50d9ae2
David Estes <davydotcom@gmail.com>
2016-03-09 10:02:07 -0500

    version bump

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,13 +14,13 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.7.0"
+		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.7.2"
 		compile 'org.mozilla:rhino:1.7R4'
 	}
 
 	plugins {
 
-		runtime ":asset-pipeline:2.7.0"
+		runtime ":asset-pipeline:2.7.2"
 		
 
 		build ':release:3.1.2', ':rest-client-builder:2.0.1', {
