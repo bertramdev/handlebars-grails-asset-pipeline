124043feaa1f990a04d548ca38a2f50de158c7f6
David Estes <davydotcom@gmail.com>
2017-01-04 09:48:16 -0500

    legacy updates

--- HandlebarsAssetPipelineGrailsPlugin.groovy
+++ HandlebarsAssetPipelineGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import asset.pipeline.handlebars.HandlebarsAssetFile
 
 class HandlebarsAssetPipelineGrailsPlugin {
 
-    def version = "2.11.0"
+    def version = "2.12.4"
 
     def grailsVersion = "2.0 > *"
     def title = "Handlebars Asset-Pipeline Plugin"
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,13 +14,13 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.11.0"
+		compile "com.bertramlabs.plugins:handlebars-asset-pipeline:2.12.4"
 		compile 'org.mozilla:rhino:1.7R4'
 	}
 
 	plugins {
 
-		runtime ":asset-pipeline:2.11.0"
+		runtime ":asset-pipeline:2.12.4"
 		
 
 		build ':release:3.1.2', ':rest-client-builder:2.0.1', {
